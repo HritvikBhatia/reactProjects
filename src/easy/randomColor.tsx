8c308057027752274f32e08a9ab40dce3756d200
vik <hritvik111@gmail.com>
2026-02-07 00:36:28 +0530

    add: random Color Generator(rgb or hex codes)

--- src/easy/randomColor.tsx
+++ src/easy/randomColor.tsx
@@ -1,30 +1,51 @@
 import { useState } from "react";
 
+type ofColor = "rgb" | "hex";
+
 function RandomColor() {
-    const [currColor, setCurrColor] = useState("ffffff")
-    const str = "abcdef0123456789";
+    const [currColor, setCurrColor] = useState("#ffffff")
+    const [_typeOfColor, setTypeOfColor ] = useState<ofColor>("hex")
     
-    const generateColor = () => {
-        
-        let newColor = ""
+
+    const randomNumberUtility = (length: number) => {
+        return Math.floor(Math.random() * length)
+    }
+    
+    const generateHexColor = () => {        
+        let newColor = "#"
+        const str = "abcdef0123456789";
+
         for(let i = 0; i < 6; i++){
-            const char = Math.floor(Math.random() * str.length);
+            const char = randomNumberUtility(str.length)
             newColor += str[char]
         }
         setCurrColor(newColor);
 
     }
 
-    return <div className="w-full h-dvh" style={{
-        backgroundColor: `#${currColor}`
-    }}>
+    const generateRgbColor = () => {
+        const r = randomNumberUtility(256);
+        const g = randomNumberUtility(256);
+        const b = randomNumberUtility(256);
+        
+        setCurrColor(`rgb(${r},${g},${b})`)
+    }
 
-        <div onClick={generateColor} className="flex flex-col  items-center justify-around h-full">
-            <button className="border border-b-black m-2 p-2 text-lg">GenerateColor</button>
-            <h2 className="text-3xl">#{currColor}</h2>
+    return <div className="w-full h-dvh" style={{
+        backgroundColor: currColor
+    }}>
+        <div className="flex flex-col  items-center justify-around h-full">
+            <div>
+                <button className="border border-b-black m-2 p-2 text-lg" onClick={() => (setTypeOfColor("hex"), generateHexColor())} >Generate HEX Color</button>
+                <button className="border border-b-black m-2 p-2 text-lg" onClick={() => (setTypeOfColor("rgb"), generateRgbColor())} >Generate RGB Color</button>
+            </div>
+            <h2 className="text-3xl">
+                {currColor}
+            </h2>
         </div>
 
     </div>;
 }
 
 export default RandomColor;
+                                       
